2120e1ac777e78b104511b04591901a07f3d6120
edsdx <2091828474@qq.com>
2024-12-17 09:19:35 +0800

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // 当按钮被点击时，跳转到指定的 URL
+    $("#resolveButton").click(function() {
+        window.location.href = "https://www.doubao.com/chat/?channel=baidu_pz&source=db_baidu_pz_01&keywordid=weizhi7";
+    });
+});
